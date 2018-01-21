c8d015b010651dcaada239b644e9f70059d13cee
kyeru <hyungjuneim@gmail.com>
2018-01-21 22:48:22 +0900

    flask routing method

--- ngtest.py
+++ ngtest.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 #from flask.ext.triangle import Triangle
+from datetime import datetime
 
 app = Flask(__name__)
 #Triangle(app)
@@ -8,5 +9,12 @@ app = Flask(__name__)
 def default():
     return render_template('ngtest.html')
 
+@app.route('/bs')
+def bootstrap():
+    timestamp = datetime.today()
+    return render_template(
+        'bootstrap_prac.html',
+        timestamp = str(timestamp))
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(port = 5000, debug = True)
